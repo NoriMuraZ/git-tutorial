f808ac52ea0ab75ff3efce94810b18d1a9a94f8c
NoriMuraZ <99166088+NoriMuraZ@users.noreply.github.com>
2022-12-07 15:21:27 +0900

    Create users.js

---
+++ users.js
@@ -0,0 +1,13 @@
+ res.status(200).json((result:"OK",message
+                       req.params.id));
+))
+
+app.get("/api/users",async(req,response
+         var result =await User.findAll({
+          User.findAll({
+            attributes:{
+              exclude: {'updatetime'}
+            }
+          })         
+         })
+))
